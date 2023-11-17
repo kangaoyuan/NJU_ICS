106e9bfa7d245aa4f3f5dab84db4ea96b3270928
gaoyuankan <kangaoyuan@163.com>
2023-11-18 02:59:17 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 890b9361471c4c6592ec4182ce1b04a6   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.90.1-microsoft-standard-WSL2     Architecture: x86-64  02:59:17 up 12:44,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -3,48 +3,40 @@
 
 #include <common.h>
 
-
 // memory
-#define x86_IMAGE_START 0x100000
 #define x86_PMEM_BASE 0x0
+#define x86_IMAGE_START 0x100000
 
 // reg
-
 /* TODO: Re-organize the `CPU_state' structure to match the register
- * encoding scheme in i386 instruction format. For example, if we
- * access cpu.gpr[3]._16, we will get the `bx' register; if we access
- * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
- * For more details about the register encoding scheme, see i386 manual.
- */
-
+ * encoding scheme in i386 instruction format. */
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
-
-  /* Do NOT change the order of the GPRs' definitions. */
-
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
-  vaddr_t pc;
+    /* Do NOT change the order of the GPRs' definitions. */
+    union {
+        struct {
+            rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        };
+        union {
+            uint32_t _32;
+            uint16_t _16;
+            uint8_t  _8[2];
+        } gpr[8];
+    };
+    vaddr_t pc;
 } x86_CPU_state;
 
 // decode
 typedef struct {
-  bool is_operand_size_16;
-  uint8_t ext_opcode;
-  const rtlreg_t *mbase;
-  rtlreg_t mbr;
-  word_t moff;
+    bool            is_operand_size_16;
+    uint8_t         ext_opcode;
+    const rtlreg_t* mbase;
+    rtlreg_t        mbr;
+    word_t          moff;
 } x86_ISADecodeInfo;
 
-#define suffix_char(width) ((width) == 4 ? 'l' : ((width) == 1 ? 'b' : ((width) == 2 ? 'w' : '?')))
-#define isa_vaddr_check(vaddr, type, len) (MEM_RET_OK)
 #define x86_has_mem_exception() (false)
+#define isa_vaddr_check(vaddr, type, len) (MEM_RET_OK)
+#define suffix_char(width)                                                 \
+    ((width) == 4 ? 'l' : ((width) == 2 ? 'w' : ((width) == 1 ? 'b' : '?')))
 
 #endif
