cf8ef84c623556aecb3b38f62e052cd20fea275d
gaoyuankan <kangaoyuan@163.com>
2023-11-29 15:05:57 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 181f233f5cf84a94b553e71ab590036d   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  15:05:57 up  3:11,  1 user,  load average: 0.14, 0.03, 0.01

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -5,7 +5,7 @@ static inline def_EHelper(add) {
   rtl_add(s, s0, ddest, dsrc1);
   rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s1);
-  rtl_is_add_carry(s, s1, s0, dsrc1);
+  rtl_is_add_carry(s, s1, ddest, dsrc1);
   rtl_set_CF(s, s1);
   rtl_update_ZFSF(s, s0, id_dest->width);
   operand_write(s, id_dest, s0);
