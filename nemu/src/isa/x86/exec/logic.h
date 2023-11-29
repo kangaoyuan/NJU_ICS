ae4c78ab6d1f0fb649e98ae3bcecf6bf5458b44d
gaoyuankan <kangaoyuan@163.com>
2023-11-29 18:15:23 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 181f233f5cf84a94b553e71ab590036d   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  18:15:23 up  6:20,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -1,49 +1,56 @@
 #include "cc.h"
 #include "../local-include/decode.h"
 
-static inline def_EHelper(test) {
-  TODO();
-  print_asm_template2(test);
+static inline def_EHelper(setcc) {
+    uint32_t cc = s->opcode & 0xf;
+    rtl_setcc(s, ddest, cc);
+    operand_write(s, id_dest, ddest);
+    print_asm("set%s %s", get_cc_name(cc), id_dest->str);
 }
 
-static inline def_EHelper(setcc) {
-  uint32_t cc = s->opcode & 0xf;
-  rtl_setcc(s, ddest, cc);
-  operand_write(s, id_dest, ddest);
-
-  print_asm("set%s %s", get_cc_name(cc), id_dest->str);
+static inline def_EHelper(test) {
+    rtl_and(s, s0, ddest, dsrc1);
+    rtl_update_ZFSF(s, s0, id_dest->width);
+    rtl_set_CF(s, rz);
+    rtl_set_OF(s, rz);
+    print_asm_template2(test);
 }
 
 static inline def_EHelper(and) {
-  rtl_and(s,s0,ddest,dsrc1);
-  operand_write(s,id_dest,s0);
-  rtl_set_CF(s, rz);
-  rtl_set_OF(s, rz);
-  rtl_update_ZFSF(s,s0,id_dest->width);
-  print_asm_template2(and);
+    rtl_and(s, s0, ddest, dsrc1);
+    operand_write(s, id_dest, s0);
+    rtl_update_ZFSF(s, s0, id_dest->width);
+    rtl_set_CF(s, rz);
+    rtl_set_OF(s, rz);
+    print_asm_template2(and);
 }
 
 static inline def_EHelper(or) {
-  rtl_or(s, s0, ddest, dsrc1);
-  operand_write(s, id_dest, s0);
-  rtl_set_CF(s, rz);
-  rtl_set_OF(s, rz);
-  rtl_update_ZFSF(s,s0,id_dest->width);
-  print_asm_template2(or);
+    rtl_or(s, s0, ddest, dsrc1);
+    operand_write(s, id_dest, s0);
+    rtl_update_ZFSF(s, s0, id_dest->width);
+    rtl_set_CF(s, rz);
+    rtl_set_OF(s, rz);
+    print_asm_template2(or);
 }
 
 static inline def_EHelper(xor) {
-  rtl_xor(s, s0, ddest, dsrc1);
-  operand_write(s, id_dest, s0);
-  rtl_set_CF(s, rz);
-  rtl_set_OF(s, rz);
-  rtl_update_ZFSF(s,s0,id_dest->width);
-  print_asm_template2(xor);
+    rtl_xor(s, s0, ddest, dsrc1);
+    operand_write(s, id_dest, s0);
+    rtl_update_ZFSF(s, s0, id_dest->width);
+    rtl_set_CF(s, rz);
+    rtl_set_OF(s, rz);
+    print_asm_template2(xor);
 }
 
 static inline def_EHelper(not) {
-  TODO();
-  print_asm_template1(not);
+    rtl_li(s, s0, BITMASK(8 * id_dest->width));
+    rtl_xor(s, s1, ddest, s0);
+    operand_write(s, id_dest, s1);
+    rtl_update_ZFSF(s, s1, id_dest->width);
+    rtl_set_CF(s, rz);
+    rtl_set_OF(s, rz);
+    print_asm_template1(not);
 }
 
 static inline def_EHelper(sar) {
