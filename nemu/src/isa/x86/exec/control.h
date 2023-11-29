2dd6c0ef2fea9d9e1a5f21e3477d1080dba29d37
gaoyuankan <kangaoyuan@163.com>
2023-11-30 01:55:34 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 181f233f5cf84a94b553e71ab590036d   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  01:55:34 up 14:00,  1 user,  load average: 0.14, 0.06, 0.01

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -34,9 +34,9 @@ static inline def_EHelper(call) {
 }
 
 static inline def_EHelper(ret) {
-  rtl_pop(s, &s->jmp_pc);
-  rtl_j(s, s->jmp_pc);
-  print_asm("ret");
+    rtl_pop(s, &s->jmp_pc);
+    rtl_j(s, s->jmp_pc);
+    print_asm("ret");
 }
 
 static inline def_EHelper(call_rm) {
