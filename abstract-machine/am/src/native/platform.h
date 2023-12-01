e174d5ee4616d53bd095ea17010a293ac245ce49
gaoyuankan <kangaoyuan@163.com>
2023-12-01 21:32:22 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: cda031a6af66494caaf7fca63d01302b   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  21:32:22 up  9:40,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/native/platform.h
+++ abstract-machine/am/src/native/platform.h
@@ -14,15 +14,13 @@ void __am_init_timer_irq();
 void __am_pmem_map(void *va, void *pa, int prot);
 void __am_pmem_unmap(void *va);
 
-#define MY_SIGSTACK_SIZE (SIGSTKSZ)
-
 // per-cpu structure
 typedef struct {
   void *vm_head;
   uintptr_t ksp;
   int cpuid;
   Event ev; // similar to cause register in mips/riscv
-  uint8_t sigstack[MY_SIGSTACK_SIZE];
+  uint8_t sigstack[8192];
 } __am_cpu_t;
 extern __am_cpu_t *__am_cpu_struct;
 #define thiscpu __am_cpu_struct
