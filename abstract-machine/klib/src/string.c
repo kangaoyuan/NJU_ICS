2c4ad0ef326acca1fdfea926bafd2a36d4dd3ef8
gaoyuankan <kangaoyuan@163.com>
2023-12-01 16:54:47 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: cda031a6af66494caaf7fca63d01302b   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:54:47 up  5:02,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -3,44 +3,100 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  return 0;
+size_t strlen(const char* s) {
+    int cnt = 0;
+    while (*s) {
+        ++s;
+        ++cnt;
+    }
+    return cnt;
 }
 
-char *strcpy(char* dst,const char* src) {
-  return NULL;
+char* strcpy(char* dst, const char* src) {
+    char *result = dst;
+    // while(*dst++ = *src++) ; // equalvalent to below two statements.
+    while (*src) {
+        *dst++ = *src++;
+    }
+    *dst = '\0';
+    return result;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+    char *result = dst;
+    while (*src && n > 0) {
+        --n;
+        *dst++ = *src++;
+    }
+    *dst = '\0';
+    return result;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+    char *result = dst;
+    int length = strlen(dst);
+    for(dst += length; *src; ++dst){
+         *dst = *src; 
+    }
+    *dst = '\0';
+    return result;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+    while(*s1 == *s2 && *s1 != '\0' && *s2 != '\0'){
+        ++s1;
+        ++s2;
+    }
+    return *s1 - *s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+    while(n > 0){
+        if(*s1 != *s2){
+            return *s1 - *s2; 
+        }
+        --n;
+        ++s1;
+        ++s2;
+    }
+    return 0;
 }
 
-void* memset(void* v,int c,size_t n) {
-  return NULL;
+void* memset(void* v, int c, size_t n) {
+    for(uint8_t *s = v; n > 0; ++s, --n){
+        *s = c; 
+    }
+    return v;
 }
 
-void* memmove(void* dst,const void* src,size_t n) {
-  return NULL;
+void* memmove(void* dst, const void* src, size_t n) {
+    uint8_t* udst = dst;
+    const uint8_t* usrc = src;
+    while(n > 0) {
+        --n;
+        *udst++ = *usrc++;
+    }
+    return dst;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+    for(uint8_t *dst = out; n > 0; --n){
+        *dst++ = *(const uint8_t*)in++; 
+    }
+    return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  return 0;
+   const uint8_t* us1 = (const uint8_t*)s1;
+   const uint8_t* us2 = (const uint8_t*)s2;
+    while(n > 0){
+        if(*us1 != *us2)
+            return *us1 - *us2;
+        --n;
+        ++us1;
+        ++us2;
+    }
+    return 0;
 }
 
 #endif
