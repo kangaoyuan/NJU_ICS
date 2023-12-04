854d6747b44b0663a3506cd214442da7952141a4
gaoyuankan <kangaoyuan@163.com>
2023-12-04 14:54:45 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: e0c9973930e4405aa003dc78d6e9e66f   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  14:54:45 up  3:52,  1 user,  load average: 0.10, 0.08, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,6 +6,20 @@
 #define MAX_BUF 2048
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+
+
+#define BUFFER_SIZE (400)
+
+static int isdigit(char c){
+  return '0' <= c && c <= '9';
+}
+
+static int islower(char c){
+  return 'a' <= c && c <= 'z';
+}
+
+
+
 int printf(const char *fmt, ...) {
     static char ans[MAX_BUF] = {};
     va_list ap;
@@ -75,78 +89,236 @@ void xtoa(unsigned int n, char* buf) {
 }
 
 int vsnprintf(char* out, size_t n, const char* fmt, va_list ap) {
-    size_t cnt = 0;
-    bool flag = true;
-    memset(out, 0, MAX_BUF);
+    //size_t cnt = 0;
+    //bool flag = true;
+    //memset(out, 0, MAX_BUF);
 
-    for (size_t num = 0; *fmt && flag ; ++fmt) {
-        char cur = *fmt;
-        // %[flags][width][.precision][length]specifier
-        if (cur == '%') {
-            ++fmt;
-            char c = '\0';
-            char *s = NULL;
-            char buf[MAX_BUF/2] = {0};
-            switch (*fmt) {
-            case 'c':
-                c = va_arg(ap, int);
-                *out++ = c;
-                cnt++;
-                if(++num >= n)
-                    flag = false;
-                break;
-            case 'd':
-                n = va_arg(ap, int);
-                if (n < 0){
-                    num++;
-                    n = -n;
-                    *out++ = '-';
-                }
-                itoa(n, buf);
-                memcpy(out, buf, strlen(buf));
-                out += strlen(buf);
-                num += strlen(buf);
-                cnt++;
-                if(++num >= n)
-                    flag = false;
-                break;
-            // x is unsigned, no need to judge minus.
-            case 'x':
-                n = va_arg(ap, int);
-                xtoa(n, buf);
-                memcpy(out, buf, strlen(buf));
-                out += strlen(buf);
-                num += strlen(buf);
-                cnt++;
-                if(++num >= n)
-                    flag = false;
-                break;
-            case 's':
-                s = va_arg(ap, char*);
-                memcpy(out, s, strlen(s));
-                out += strlen(s);
-                num += strlen(s);
-                cnt++;
-                if(++num >= n)
-                    flag = false;
-                break;
-            case '%':
-                *out++ = '%';
-                cnt++;
-                if(++num >= n)
-                    flag = false;
-                break;
-            }
-        } else {
-            *out++ = cur;
-            cnt++;
-            if(++num >= n)
-                flag = false; 
-            break;  
+    //for (size_t num = 0; *fmt && flag ; ++fmt) {
+    //    char cur = *fmt;
+    //    // %[flags][width][.precision][length]specifier
+    //    if (cur == '%') {
+    //        ++fmt;
+    //        char c = '\0';
+    //        char *s = NULL;
+    //        char buf[MAX_BUF/2] = {0};
+    //        switch (*fmt) {
+    //        case 'c':
+    //            c = va_arg(ap, int);
+    //            *out++ = c;
+    //            cnt++;
+    //            if(++num >= n)
+    //                flag = false;
+    //            break;
+    //        case 'd':
+    //            n = va_arg(ap, int);
+    //            if (n < 0){
+    //                num++;
+    //                n = -n;
+    //                *out++ = '-';
+    //            }
+    //            itoa(n, buf);
+    //            memcpy(out, buf, strlen(buf));
+    //            out += strlen(buf);
+    //            num += strlen(buf);
+    //            cnt++;
+    //            if(++num >= n)
+    //                flag = false;
+    //            break;
+    //        // x is unsigned, no need to judge minus.
+    //        case 'x':
+    //            n = va_arg(ap, int);
+    //            xtoa(n, buf);
+    //            memcpy(out, buf, strlen(buf));
+    //            out += strlen(buf);
+    //            num += strlen(buf);
+    //            cnt++;
+    //            if(++num >= n)
+    //                flag = false;
+    //            break;
+    //        case 's':
+    //            s = va_arg(ap, char*);
+    //            memcpy(out, s, strlen(s));
+    //            out += strlen(s);
+    //            num += strlen(s);
+    //            cnt++;
+    //            if(++num >= n)
+    //                flag = false;
+    //            break;
+    //        case '%':
+    //            *out++ = '%';
+    //            cnt++;
+    //            if(++num >= n)
+    //                flag = false;
+    //            break;
+    //        }
+    //    } else {
+    //        *out++ = cur;
+    //        cnt++;
+    //        if(++num >= n)
+    //            flag = false; 
+    //        break;  
+    //    }
+    //}
+
+    //return cnt++;
+    static char vsn_buffer[BUFFER_SIZE]; // Pay attetion to this STATIC array
+  static char *presentation = "0123456789abcdef";
+  char* buffer = vsn_buffer;
+  char *outp = out;
+  int ret = 0;
+  const char *ori = fmt;
+  while(*fmt){
+    char cur = *fmt;
+    if(cur == '%'){
+      ++fmt;
+      char pre = '\0';
+      int mnWidth = 0, outWidth = 0;
+      char type = '\0';
+      if(isdigit(*fmt)){
+        switch(*fmt){
+          case '0':
+            pre = '0';
+            fmt++;
+            break;
+          default:
+            assert(0);
+            break;
         }
+        while(isdigit(*fmt)){
+          mnWidth = mnWidth*10+((*fmt)-'0');
+          fmt++;
+        }
+      }
+      if(islower(*fmt)){
+        type = *fmt;
+      }
+      char *cp = &buffer[BUFFER_SIZE-1];
+      *cp = '\0';
+      switch(type){
+        case 'd':
+        {
+          int32_t var = va_arg(ap, int32_t);
+          int isneg = 0;
+          if(var < 0){
+            isneg = 1;
+            var = -var;
+          }
+          do{
+            *(--cp) = presentation[var % 10];
+            outWidth++;
+            var /= 10;
+          }while (var);
+          if(isneg){
+            *(--cp) = '-';
+            outWidth++;
+          }
+          break;
+        }
+        case 'u':
+        {
+          uint32_t var = va_arg(ap, uint32_t);
+          do{
+            *(--cp) = presentation[var % 10];
+            outWidth++;
+            var /= 10;
+          }while (var);
+          break;
+        }
+        case 'p':
+        {
+          // Do not use uint32_t uint64_t but use unsigned long to support -m64 -m32
+          unsigned long var = (unsigned long)(va_arg(ap, void *));
+          do{
+            *(--cp) = presentation[var % 16];
+            outWidth++;
+            var /= 16;
+          }while (var);
+          *(--cp) = 'x';
+          outWidth++;
+          *(--cp) = '0';
+          outWidth++;
+          break;
+        }
+        case 'x':
+        {
+          uint32_t var = va_arg(ap, uint32_t);
+          do{
+            *(--cp) = presentation[var % 16];
+            outWidth++;
+            var /= 16;
+          }while (var);
+          break;
+        }
+        case 'f':
+        {
+          double var = va_arg(ap, double);
+          int32_t _int = (int32_t)(var*10000);// remain 4 bit
+          int isneg = 0;
+          if(_int < 0){
+            isneg = 1;
+            _int = -_int;
+          }
+          for(int i=0;i<4;i++){
+            *(--cp) = presentation[_int % 10];
+            outWidth++;
+            _int /= 10;
+          }
+          *(--cp) = '.';
+          outWidth++;
+          do{
+            *(--cp) = presentation[_int % 10];
+            outWidth++;
+            _int /= 10;
+          }while (_int);
+          if(isneg){
+            *(--cp) = '-';
+            outWidth++;
+          }
+          break;
+        }
+        case 's':
+        {
+          char *sb = va_arg(ap, char*);
+          char *se = sb;
+          while(*se)
+            se++;
+          while(se != sb){
+            *(--cp) = *(--se);
+            outWidth++;
+          }
+          break;
+        }
+        default:
+          putch('!');
+          while(*ori){
+            putch(*ori);
+            ori++;
+          }
+          assert(0);
+          break;
+      }
+      if(pre!='\0'){
+        while(outWidth<mnWidth){
+          *(--cp) = pre;
+          outWidth++;
+        }
+      }
+      while(*cp){
+        *outp = *cp;
+        if(++ret == n)break;
+        outp++;
+        cp++;
+      }
     }
-
-    return cnt++;
+    else{
+      *outp = cur;
+      outp++;
+      if(++ret == n)break;
+    }
+    fmt++;
+  }  
+  *outp = '\0';
+  return ret;
 }
 
 #endif
