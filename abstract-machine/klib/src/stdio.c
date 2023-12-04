18f5a1925b696edc52cafecc9d0d20c44942f8fa
gaoyuankan <kangaoyuan@163.com>
2023-12-04 14:05:52 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: e0c9973930e4405aa003dc78d6e9e66f   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  14:05:52 up  3:03,  1 user,  load average: 0.11, 0.06, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,22 +3,19 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 
+#define MAX_BUF 2048
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-    char ans[2000] = {};
+    static char ans[MAX_BUF] = {};
     va_list ap;
     va_start(ap, fmt);
-    int ret = vsprintf(ans, fmt, ap);
+    int ret = vsnprintf(ans, MAX_BUF, fmt, ap);
     va_end(ap);
     for(int i = 0; ans[i]; ++i){
         putch(ans[i]); 
     }
     return ret;
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
@@ -30,11 +27,126 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+    va_list arg;
+    va_start(arg, fmt);
+    int ret = vsnprintf(out, n, fmt, arg);
+    va_end(arg);
+    return ret;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  return 0;
+int vsprintf(char* out, const char* fmt, va_list ap) {
+    return vsnprintf(out, -1, fmt, ap);
+}
+
+void itoa(unsigned int n, char* buf) {
+    if (n < 10) {
+        buf[0] = n + '0';
+        buf[1] = '\0';
+        return;
+    }
+
+    itoa(n / 10, buf);
+
+    int i = strlen(buf);
+    buf[i] = (n % 10) + '0';
+    buf[i + 1] = '\0';
+}
+
+void xtoa(unsigned int n, char* buf) {
+    if (n < 10) {
+        buf[0] = n + '0';
+        buf[1] = '\0';
+    } else if (n < 16) {
+        buf[0] = n - 10 + 'a';
+        buf[1] = '\0';
+    }
+    return;
+
+    xtoa(n / 16, buf);
+
+    int i = strlen(buf);
+    if (n % 16 < 10) {
+        buf[i] = (n % 16) + '0';
+        buf[i + 1] = '\0';
+    } else if (n < 16) {
+        buf[i] = (n % 16) - 10 + 'a';
+        buf[i + 1] = '\0';
+    }
+}
+
+int vsnprintf(char* out, size_t n, const char* fmt, va_list ap) {
+    size_t cnt = 0;
+    bool flag = true;
+    memset(out, 0, MAX_BUF);
+
+    for (size_t num = 0; *fmt && flag ; ++fmt) {
+        char cur = *fmt;
+        // %[flags][width][.precision][length]specifier
+        if (cur == '%') {
+            ++fmt;
+            char c = '\0';
+            char *s = NULL;
+            char buf[MAX_BUF/2] = {0};
+            switch (*fmt) {
+            case 'c':
+                c = va_arg(ap, int);
+                *out++ = c;
+                cnt++;
+                if(++num >= n)
+                    flag = false;
+                break;
+            case 'd':
+                n = va_arg(ap, int);
+                if (n < 0){
+                    num++;
+                    n = -n;
+                    *out++ = '-';
+                }
+                itoa(n, buf);
+                memcpy(out, buf, strlen(buf));
+                out += strlen(buf);
+                num += strlen(buf);
+                cnt++;
+                if(++num >= n)
+                    flag = false;
+                break;
+            // x is unsigned, no need to judge minus.
+            case 'x':
+                n = va_arg(ap, int);
+                xtoa(n, buf);
+                memcpy(out, buf, strlen(buf));
+                out += strlen(buf);
+                num += strlen(buf);
+                cnt++;
+                if(++num >= n)
+                    flag = false;
+                break;
+            case 's':
+                s = va_arg(ap, char*);
+                memcpy(out, s, strlen(s));
+                out += strlen(s);
+                num += strlen(s);
+                cnt++;
+                if(++num >= n)
+                    flag = false;
+                break;
+            case '%':
+                *out++ = '%';
+                cnt++;
+                if(++num >= n)
+                    flag = false;
+                break;
+            }
+        } else {
+            *out++ = cur;
+            cnt++;
+            if(++num >= n)
+                flag = false; 
+            break;  
+        }
+    }
+
+    return cnt++;
 }
 
 #endif
