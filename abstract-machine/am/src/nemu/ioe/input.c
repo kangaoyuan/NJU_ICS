32c4f71a6b6d1be17d79e79989a481932e682114
gaoyuankan <kangaoyuan@163.com>
2023-12-05 23:26:08 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 746bc261292343b7bdb71f120950b5fb   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  23:26:08 up 10:13,  1 user,  load average: 0.01, 0.09, 0.05

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -7,6 +7,4 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T* kbd) {
     uint32_t data = inl(KBD_ADDR);
     kbd->keycode = data & ~KEYDOWN_MASK;
     kbd->keydown = (data & KEYDOWN_MASK) > 0;
-
-    kbd->keycode = AM_KEY_NONE;
 }
