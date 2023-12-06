0bc94f4aee73fc781b9972db28ab0b1021ca8ee4
gaoyuankan <kangaoyuan@163.com>
2023-12-07 05:30:34 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 9f785775582547d1a3f23714c5b1c04a   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  05:30:34 up  3:11,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -11,11 +11,10 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-    int       i;
     int       w = W;  // TODO: get the correct width
     int       h = H;  // TODO: get the correct height
     uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-    for (i = 0; i < w * h; i++)
+    for (int i = 0; i < w * h; i++)
         fb[i] = i;
     outl(SYNC_ADDR, 1);
 }
@@ -39,7 +38,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T* ctl) {
         base += w;
     }
     if (ctl->sync) {
-        outl(SYNC_ADDR, 1);
+        //outl(SYNC_ADDR, 1);
     }
 }
 
