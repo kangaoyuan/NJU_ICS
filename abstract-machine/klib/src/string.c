8698437b8c80878dd5f6fd4f46e7feb4261d8973
gaoyuankan <kangaoyuan@163.com>
2023-12-07 17:26:39 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: c13fe8df30bd4164a2cfa7128792b6a0   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  17:26:39 up  1:34,  1 user,  load average: 0.09, 0.07, 0.08

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -13,14 +13,13 @@ size_t strlen(const char* s) {
 }
 
 char* strcpy(char* dst, const char* src) {
-    /*  char *result = dst;
-        while(*dst++ = *src++) ; // equalvalent to below two statements.
-        while (*src) {
-            *dst++ = *src++;
-        }
-        *dst = '\0';
-        return result;  */
-    return memmove(dst, src, strlen(src)+1);
+    char *result = dst;
+    // while(*dst++ = *src++) ; // equalvalent to below two statements.
+    while (*src) {
+        *dst++ = *src++;
+    }
+    *dst = '\0';
+    return result;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
@@ -29,27 +28,22 @@ char* strncpy(char* dst, const char* src, size_t n) {
         --n;
         *dst++ = *src++;
     }
-    while(n > 0){
-        --n;
-        *dst++ = '\0'; 
-    }
+    *dst = '\0';
     return result;
 }
 
 char* strcat(char* dst, const char* src) {
-    /*  char *result = dst;
-        dst += strlen(dst);
-        while(*src){
-             *dst++ = *src++; 
-        }
-        *dst = '\0';
-        return result; */
-    strcpy (dst + strlen (dst), src);
-    return dst;
+    char *result = dst;
+    int length = strlen(dst);
+    for(dst += length; *src; ++dst){
+         *dst = *src; 
+    }
+    *dst = '\0';
+    return result;
 }
 
 int strcmp(const char* s1, const char* s2) {
-    while(*s1 == *s2 && *s1 != '\0'){
+    while(*s1 == *s2 && *s1 != '\0' && *s2 != '\0'){
         ++s1;
         ++s2;
     }
@@ -57,44 +51,15 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-    unsigned char c1 = '\0';
-  unsigned char c2 = '\0';
-
-  if (n >= 4)
-    {
-      size_t n4 = n >> 2;
-      do
-    {
-      c1 = (unsigned char) *s1++;
-      c2 = (unsigned char) *s2++;
-      if (c1 == '\0' || c1 != c2)
-        return c1 - c2;
-      c1 = (unsigned char) *s1++;
-      c2 = (unsigned char) *s2++;
-      if (c1 == '\0' || c1 != c2)
-        return c1 - c2;
-      c1 = (unsigned char) *s1++;
-      c2 = (unsigned char) *s2++;
-      if (c1 == '\0' || c1 != c2)
-        return c1 - c2;
-      c1 = (unsigned char) *s1++;
-      c2 = (unsigned char) *s2++;
-      if (c1 == '\0' || c1 != c2)
-        return c1 - c2;
-    } while (--n4 > 0);
-      n &= 3;
+    while(n > 0){
+        if(*s1 != *s2){
+            return *s1 - *s2; 
+        }
+        --n;
+        ++s1;
+        ++s2;
     }
-
-  while (n > 0)
-    {
-      c1 = (unsigned char) *s1++;
-      c2 = (unsigned char) *s2++;
-      if (c1 == '\0' || c1 != c2)
-    return c1 - c2;
-      n--;
-    }
-
-  return c1 - c2;
+    return 0;
 }
 
 void* memset(void* v, int c, size_t n) {
@@ -105,21 +70,12 @@ void* memset(void* v, int c, size_t n) {
 }
 
 void* memmove(void* dst, const void* src, size_t n) {
-    unsigned char*       d = dst;
-    const unsigned char* s = src;
-
-    if (d < s || d >= s + n) {
-        while (n--) {
-            *d++ = *s++;
-        }
-    } else {
-        d += n - 1;
-        s += n - 1;
-        while (n--) {
-            *d-- = *s--;
-        }
+    uint8_t* udst = dst;
+    const uint8_t* usrc = src;
+    while(n > 0) {
+        --n;
+        *udst++ = *usrc++;
     }
-
     return dst;
 }
 
@@ -131,8 +87,8 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-   const uint8_t* us1 = s1;
-   const uint8_t* us2 = s2;
+   const uint8_t* us1 = (const uint8_t*)s1;
+   const uint8_t* us2 = (const uint8_t*)s2;
     while(n > 0){
         if(*us1 != *us2)
             return *us1 - *us2;
