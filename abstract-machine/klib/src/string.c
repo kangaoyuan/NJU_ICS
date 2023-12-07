b12250b941c6942b1f8d9ee13a0b690f19715189
gaoyuankan <kangaoyuan@163.com>
2023-12-07 17:24:08 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: c13fe8df30bd4164a2cfa7128792b6a0   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  17:24:08 up  1:31,  1 user,  load average: 0.04, 0.08, 0.08

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -57,15 +57,44 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-    while(n > 0){
-        if(*s1 != *s2){
-            return *s1 - *s2; 
-        }
-        --n;
-        ++s1;
-        ++s2;
+    unsigned char c1 = '\0';
+  unsigned char c2 = '\0';
+
+  if (n >= 4)
+    {
+      size_t n4 = n >> 2;
+      do
+    {
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0' || c1 != c2)
+        return c1 - c2;
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0' || c1 != c2)
+        return c1 - c2;
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0' || c1 != c2)
+        return c1 - c2;
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0' || c1 != c2)
+        return c1 - c2;
+    } while (--n4 > 0);
+      n &= 3;
     }
-    return 0;
+
+  while (n > 0)
+    {
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0' || c1 != c2)
+    return c1 - c2;
+      n--;
+    }
+
+  return c1 - c2;
 }
 
 void* memset(void* v, int c, size_t n) {
