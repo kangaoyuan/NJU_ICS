80ad89845a5605a5ae3a8e256954091319e0c429
gaoyuankan <kangaoyuan@163.com>
2023-12-07 17:14:13 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: c13fe8df30bd4164a2cfa7128792b6a0   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  17:14:13 up  1:21,  1 user,  load average: 0.00, 0.04, 0.07

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -49,20 +49,11 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
- //   const unsigned char *s1 = (const unsigned char *) p1;
-//   const unsigned char *s2 = (const unsigned char *) p2;
-  unsigned char c1, c2;
-
-  do
-    {
-      c1 = (unsigned char) *s1++;
-      c2 = (unsigned char) *s2++;
-      if (c1 == '\0')
-    return c1 - c2;
+    while(*s1 == *s2 && *s1 != '\0'){
+        ++s1;
+        ++s2;
     }
-  while (c1 == c2);
-
-  return c1 - c2;
+    return *s1 - *s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
