bd6b5b62a3d2944263a69471c60624b7002ff1fa
gaoyuankan <kangaoyuan@163.com>
2023-12-18 00:02:34 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 7cca03a459954c6cb05d0284bf1e9123   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  00:02:34 up  9:52,  1 user,  load average: 0.02, 0.01, 0.00

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -10,12 +10,13 @@ void raise_intr(DecodeExecState* s, word_t NO, vaddr_t ret_addr) {
     rtl_push(s, &cpu.cs);
     rtl_push(s, &ret_addr);  // rtl_push(&cpu.eip);
 
-    printf("cpu.idtr_limit%u\n", cpu.idtr_limit);
+    printf("cpu.idtr_limit = %u\n", cpu.idtr_limit);
     vaddr_t offset = cpu.idtr_base + 8 * NO;
     Assert(NO < cpu.idtr_limit, "Bigger than IDTR limit.");
     //GateDesc32 gate;
     word_t gate_low = vaddr_read(offset, 2);
     word_t gate_high = vaddr_read(offset + 6, 2);
+    printf("can i get here>\n");
 
     // gate.offset_31_16 << 16 | gate.offset_15_0 
     rtl_j(s, gate_high << 16 | gate_low);
