02c01ff492348692ea885ac01ed4f14a79837b75
gaoyuankan <kangaoyuan@163.com>
2023-12-19 12:57:12 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: b21b7898473d43c2a022fdfdba9a968c   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  12:57:13 up 6 min,  1 user,  load average: 0.00, 0.02, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,10 +9,40 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t get_ramdisk_size();
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB* pcb, const char* filename) {
-    TODO();
-    //assert(*(uint32_t *)elf->e_ident == 0xBadC0de);
-    return 0;
+    Elf_Ehdr* elf_header = NULL;
+
+    size_t  offset = ramdisk_read(elf_header, 0, sizeof(Elf_Ehdr));
+    assert(offset == sizeof(Elf_Ehdr));
+
+    // Attention: . -> () [] have higher precedence than () for casting.
+    assert(*(uint32_t*)elf_header->e_ident == 0x464c457f);
+
+    Elf_Phdr elf_program_header[elf_header->e_phnum];
+    offset = ramdisk_read(elf_program_header, elf_header->e_phoff,
+                          sizeof(Elf_Phdr) * elf_header->e_phnum);
+    assert(offset == sizeof(Elf_Phdr) * elf_header->e_phnum);
+    
+    for (int i = 0; i < elf_header->e_phnum; i++) {
+        // only load PT_LOAD type
+        if (elf_program_header[i].p_type == PT_LOAD) {
+            ramdisk_read((void*)elf_program_header[i].p_vaddr,
+                         elf_program_header[i].p_offset,
+                         elf_program_header[i].p_memsz);
+            // clear the [Virtual Address + File Size, Virtual Address +
+            // Memory Size)
+            memset((void*)(elf_program_header[i].p_vaddr +
+                           elf_program_header[i].p_filesz),
+                   0,
+                   elf_program_header[i].p_memsz -
+                       elf_program_header[i].p_filesz);
+        }
+    }
+    return elf_header->e_entry;
 }
 
 void naive_uload(PCB* pcb, const char* filename) {
