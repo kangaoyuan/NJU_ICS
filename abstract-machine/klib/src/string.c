bddd584e61e3d6e62696475b6f75d5b1f3433a14
gaoyuankan <kangaoyuan@163.com>
2023-12-21 01:39:05 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 0e90e2262ad741b8a061fe80d86917cf   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  01:39:05 up 10:55,  1 user,  load average: 0.08, 0.02, 0.01

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -3,56 +3,61 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t i = 0;;
-  while(s[i] != '\0') {i++;}
+  size_t i = 0;
+  while(s[i] != '\0')
+      i++;
   return i;
 }
 
-char *strcpy(char* dst,const char* src) {
-  size_t i = 0;
-  while(src[i] != '\0')
-  {
-    dst[i] = src[i];
-    i++;
-  }
-  dst[i] = '\0';
-  return dst;
-}
-
-char* strncpy(char* dst, const char* src, size_t n) {
-  size_t i;
-  for(i = 0;i < n&& src[i] != '\0';i++)
-    dst[i] = src[i];
-  for(; i < n;i ++)
+char* strcpy(char* dst, const char* src) {
+    size_t i = 0;
+    while (src[i] != '\0') {
+        dst[i] = src[i];
+        i++;
+    }
     dst[i] = '\0';
-  return dst;
-}
-
-char* strcat(char* dst, const char* src) {
-  size_t i = 0,j = 0;;
-  while(dst[i] != '\0')
-  {
-    i++;
-  }
-  while(src[j] != '\0')
-  {
-    dst[i] = src[j];
-    i++;
-    j++;
-  }
-  dst[i] = '\0';
-  return dst;
+    return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  char const *p1 = s1,*p2 = s2;
-  while(*p1 == *p2 && *p1 != '\0' && *p2 != '\0')
-  {
-    //printf("%c %c\n",*p1,*p2);
-    p1++;
-    p2++;
-  }
-  return *p1-*p2;
+    const char *p1 = s1, *p2 = s2;
+    while (*p1 == *p2 && *p1 != '\0' && *p2 != '\0') {
+        // printf("%c %c\n",*p1,*p2);
+        p1++;
+        p2++;
+    }
+    return *p1 - *p2;
+}
+
+char* strcat(char* dst, const char* src) {
+    size_t i = strlen(dst), j = 0;
+    while (src[j] != '\0') {
+        dst[i++] = src[j++];
+    }
+    dst[i] = '\0';
+    return dst;
+}
+
+size_t strnlen(const char*s, size_t n) {
+    size_t i = 0;
+    while(n && s[i]){
+        i++;
+        n--; 
+    }
+    return i;
+}
+
+char* strncpy(char* dst, const char* src, size_t n) {
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++)
+        dst[i] = src[i];
+    while (i < n)
+        dst[i++] = '\0';
+    return dst;
+ /* size_t size = strnlen(src, n);
+    if (size != n)
+        memset(dst + size, '\0', n - size);
+    return memcpy(dst, src, size); */
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
