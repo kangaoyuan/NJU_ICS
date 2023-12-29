23f41a438f698cfc1573f56c451bfa5a9ba3171e
gaoyuankan <kangaoyuan@163.com>
2023-12-29 19:58:07 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: fff9a58cfa8745249e9aac25c2d009bd   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  19:58:07 up  8:56,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -34,6 +34,7 @@ void send_key(uint8_t scancode, bool is_keydown) {
     if (nemu_state.state == NEMU_RUNNING && keymap[scancode] != _KEY_NONE) {
         uint32_t am_scancode =
             keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
+        // key_enqueue
         key_queue[key_r] = am_scancode;
         key_r = (key_r + 1) % KEY_QUEUE_LEN;
         Assert(key_r != key_f, "key queue overflow!");
