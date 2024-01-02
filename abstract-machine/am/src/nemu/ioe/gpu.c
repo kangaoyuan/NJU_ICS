c6c81296ec411e779135006001618b38b2d3704b
gaoyuankan <kangaoyuan@163.com>
2024-01-02 21:55:29 +0800

    >  run  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 927d0ba23c104a7c8765ec44b8a5a622   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  21:55:29 up  9:46,  1 user,  load average: 0.03, 0.01, 0.00

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -38,9 +38,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T* ctl) {
         memcpy(&fb[(y + j) * W + x], base, cp_bytes);
         base += w;
     }
-    if (ctl->sync) {
+    if (ctl->sync)
         outl(SYNC_ADDR, true);
-    }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T* status) {
