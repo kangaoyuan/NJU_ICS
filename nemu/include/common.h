79aa14300b9b0efcc61a2809dfb43096aefb26a4
gaoyuankan <kangaoyuan@163.com>
2024-01-03 15:42:40 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: ee508d42dedd47bd8e2f558aa473b70c   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  15:42:40 up  4:33,  1 user,  load average: 0.14, 0.72, 0.65

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -13,8 +13,8 @@
 
 #if _SHARE
 // do not enable these features while building a reference design
-#undef DIFF_TEST
 #undef DEBUG
+#undef DIFF_TEST
 #endif
 
 /* You will define this macro in PA2 */
