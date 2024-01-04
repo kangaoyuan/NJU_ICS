ac8675e68a53db58b2fea66e1f1c2163d88b52dc
tracer-ics2020 <tracer@njuics.org>
2024-01-04 16:54:10 +0800

    >  compile 191870271  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: 0df1b743e231485783df1c3eae37de37   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:54:10 up  4:08,  1 user,  load average: 0.02, 0.01, 0.00

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,95 +1,121 @@
-#include "watchpoint.h"
 #include "expr.h"
-#include<malloc.h>
+#include "watchpoint.h"
 
-#define NR_WP 35
-
-static WP wp_pool[NR_WP] = {};
-static WP *head = NULL, *free_ = NULL, *used =NULL;
-
-WP* new_wp(){
-  if(used==head)
-  {
-    used=free_;
-    used->prev=head;
-    return used;
-  }
-  else{
-    used->next=free_;
-    free_->prev=used;
-    used=used->next;
-    free_=free_->next;
-    return used;
-  }
-}
-
-void free_wp(WP *wp)
-{
-  if(wp==used)
-  {
-    free_->prev=used;
-    used->next=free_;
-    used=used->prev;
-  }
-  else{
-    if(wp->prev==head)
-    {
-      wp->next->prev=head;
-      wp->next=free_;
-      free_->prev=wp;
-      free_=free_->prev;
-      free(wp->exp);
-    }
-    else{
-      wp->prev->next=wp->next;
-      wp->next->prev=wp->prev;
-      wp->next=free_;
-      free_->prev=wp;
-      free_=free_->prev;
-      free(wp->exp);
-    }
-  }
-  
-  
-}//此处未考虑wp_pool使用完的情况，即认为free_ != NULL
-
-void display_wp_pool()
-{
-  WP* wp=used;
-  if(used==head)
-  {
-    printf("No watchpoint\n");
-  }
-  else{
-    while(wp != head)
-    {
-      printf("%d %s\n",wp->NO,wp->exp);
-      wp=wp->prev;
-    }
-  }
-}
-
-WP* all_wp()
-{
-  return used;
-}
+#define NR_WP 32
+static WP  wp_pool[NR_WP] = {};
+static WP *head = NULL, *free_ = NULL;
 
 void init_wp_pool() {
-  int i;
-  for (i = 0; i < NR_WP; i ++) {
-    wp_pool[i].NO = i;
-    wp_pool[i].next = &wp_pool[i + 1];
-  }
-  wp_pool[NR_WP - 1].next = NULL;
-  for(i = 1;i < NR_WP;i ++)
-  {
-    wp_pool[i].prev=&wp_pool[i-1];
-  }
-  wp_pool[0].prev=head;
-  head = NULL;
-  free_ = wp_pool;
-  used=NULL;
+    for (int i = 0; i < NR_WP; i++) {
+        wp_pool[i].NO = i;
+        wp_pool[i].next = &wp_pool[i + 1];
+    }
+    wp_pool[NR_WP - 1].next = NULL;
+
+    head = NULL;
+    free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
+void new_wp(char* arg) {
+    if (free_ == NULL)
+        panic("No more wp_pool memory space.");
+    // get a now node for new action from head of free_ list.
+    WP* now = free_;
+    free_ = free_->next;
+    now->next = NULL;
 
+    strcpy(now->str, arg);
+    bool     success;
+    uint32_t init = expr(now->str, &success);
+    if (success) {
+        now->val = init;
+        Log("Watchpoint %d: %s\nInitial value %u\n", now->NO, now->str,
+            now->val);
+    } else
+        panic("watchpoint expr() eval failure.");
+
+    // Attention 
+    if (head == NULL)
+        head = now;
+    else {
+        now->next = head->next;
+        head->next = now;
+    }
+    return;
+}
+
+void free_wp(int index) {
+    if (index < 0 || head == NULL)
+        assert(0);
+
+    int num = 0;
+    WP* now = head;
+    WP* target = NULL;
+    while (now) {
+        if (now->NO == index) {
+            target = now;
+            break;
+        }
+        if (now->next != NULL) {
+            num++;
+            now = now->next;
+        }
+    }
+    if (target == NULL) {
+        Log("Watchpoint %d not found!\n", index);
+        return;
+    }
+
+    WP* before;
+    WP* after = target->next;
+    if (num == 0) {
+        head = after;
+    } else {
+        now = head;
+        while (num > 1) {
+            now = now->next;
+            num--;
+        }
+        before = now;
+        before->next = after;
+    }
+
+    // Insert target node into the head of free_ list.
+    target->next = free_;
+    free_ = target;
+    free_->val = 0;
+    free_->str[0] = '\0';
+    return;
+}
+
+void wp_display() {
+    WP* now = head;
+    if (now == NULL)
+        printf("No watchpoints.\n");
+    while (now) {
+        printf("Watchpoint %d: %s = %u\n", now->NO, now->str, now->val);
+        now = now->next;
+    }
+    return;
+}
+
+bool check_wp(){
+    bool flag = false;
+    for(WP *now = head; now; now = now->next){
+        bool success;
+        uint32_t val = now->val; 
+        uint32_t real = expr(now->str, &success);
+        if (success) {
+            if (val != real) {
+                printf(
+                    "Watchpoint %d: %s\nOld value = %u\nNew value = %u\n",
+                    now->NO, now->str, val, real);
+                now->val = real;
+                flag = true;
+            }
+        } else
+            panic("watchpoint expr() eval failure.");
+    }
+    return flag;
+}
