9a5da9b89fcdc2b646a76f6f383bc1a5a18dabe5
gaoyuankan <kangaoyuan@163.com>
2024-01-05 16:19:45 +0800

    >  compile  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 98eb7e08a7344dc38e7381e71749fa48          Boot ID: b71726d4c9a3474e84834bb622f7a2c4   Virtualization: wsl Operating System: Ubuntu 22.04.2 LTS           Kernel: Linux 5.15.133.1-microsoft-standard-WSL2     Architecture: x86-64  16:19:45 up  4:13,  1 user,  load average: 0.08, 0.07, 0.08

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -11,29 +11,27 @@
 
 #if _SHARE
 // do not enable these features while building a reference design
-#undef DIFF_TEST
 #undef DEBUG
+#undef DIFF_TEST
 #endif
 
 /* You will define this macro in PA2 */
 #define HAS_IOE
 
-#include <stdint.h>
-#include <assert.h>
 #include <string.h>
-
-typedef uint8_t bool;
+#include <assert.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifdef ISA64
-typedef uint64_t word_t
-typedef int64_t sword_t
+typedef uint64_t word_t;
+typedef int64_t sword_t;
 #define FMT_WORD "0x%016lx"
 #else
 typedef uint32_t word_t;
 typedef int32_t sword_t;
 #define FMT_WORD "0x%08x"
 #endif
-
 
 typedef word_t rtlreg_t;
 typedef word_t vaddr_t;
