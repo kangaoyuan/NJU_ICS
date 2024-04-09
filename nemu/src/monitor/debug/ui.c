b10f5883d5ae516147e8a9cb8e1f06cb151ef405
tracer-ics2020 <tracer@njuics.org>
2024-04-09 17:37:34 +0800

    >  compile 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 4fb35b2893144839b04f583edf14240a   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  17:37:34 up  5:18,  1 user,  load average: 0.06, 0.02, 0.00

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -11,44 +11,44 @@ int is_batch_mode();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
-  static char *line_read = NULL;
+    static char* line_read = NULL;
 
-  if (line_read) {
-    free(line_read);
-    line_read = NULL;
-  }
+    if (line_read) {
+        //malloc() is called in readline() and returns the text of the line read, which has the final newline removed.
+        free(line_read);
+        line_read = NULL;
+    }
 
-  line_read = readline("(nemu) ");
+    line_read = readline("(nemu) ");
 
-  if (line_read && *line_read) {
-    add_history(line_read);
-  }
+    if (line_read && *line_read) {
+        add_history(line_read);
+    }
 
-  return line_read;
+    return line_read;
 }
 
-static int cmd_c(char *args) {
-  cpu_exec(-1);
-  return 0;
+static int cmd_c(char* args) {
+    cpu_exec(-1);
+    return 0;
 }
 
-
-static int cmd_q(char *args) {
-  return -1;
+static int cmd_q(char* args) {
+    return -1;
 }
 
 static int cmd_help(char *args);
 
 static struct {
-  char *name;
-  char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display informations about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
+    char* name;
+    char* description;
+    int (*handler)(char*);
+} cmd_table[] = {
+    {"help", "Display informations about all supported commands", cmd_help},
+    {"c", "Continue the execution of the program", cmd_c},
+    {"q", "Exit NEMU", cmd_q},
 
-  /* TODO: Add more commands */
+    /* TODO: Add more commands */
 
 };
 
@@ -78,39 +78,45 @@ static int cmd_help(char *args) {
 }
 
 void ui_mainloop() {
-  if (is_batch_mode()) {
-    cmd_c(NULL);
-    return;
-  }
-
-  for (char *str; (str = rl_gets()) != NULL; ) {
-    char *str_end = str + strlen(str);
-
-    /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
-
-    /* treat the remaining string as the arguments,
-     * which may need further parsing
-     */
-    char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end) {
-      args = NULL;
+    if (is_batch_mode()) {
+        cmd_c(NULL);
+        return;
     }
+
+    for (char* str; (str = rl_gets()) != NULL;) {
+        char* str_end = str + strlen(str);
+
+        /* extract the first token as the command */
+        char* cmd = strtok(str, " ");
+        if (cmd == NULL) {
+            continue;
+        }
+
+        /* treat the remaining string as the arguments,
+         * which may need further parsing
+         */
+        char* args = cmd + strlen(cmd) + 1;
+        if (args >= str_end) {
+            args = NULL;
+        }
 
 #ifdef HAS_IOE
-    extern void sdl_clear_event_queue();
-    sdl_clear_event_queue();
+        extern void sdl_clear_event_queue();
+        sdl_clear_event_queue();
 #endif
 
-    int i;
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
-        break;
-      }
-    }
+        int i;
+        for (i = 0; i < NR_CMD; i++) {
+            if (strcmp(cmd, cmd_table[i].name) == 0) {
+                if (cmd_table[i].handler(args) < 0) {
+                    return;
+                }
+                break;
+            }
+        }
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-  }
+        if (i == NR_CMD) {
+            printf("Unknown command '%s'\n", cmd);
+        }
+    }
 }
