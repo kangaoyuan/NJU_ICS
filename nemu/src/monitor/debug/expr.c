ba484f65e886f3034e73f8f8f0017524d9b69a11
tracer-ics2020 <tracer@njuics.org>
2024-04-14 19:31:08 +0800

    >  compile 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 8ecd5cc9815c4a15bf0c9046f8364b5f   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  19:31:08 up  6:54,  1 user,  load average: 0.14, 0.08, 0.02

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -291,8 +291,8 @@ static uint32_t eval(int left, int right) {
         case '*':
             return val1 * val2;
         case '/':
-            if(val2 == 0)
-                panic("eval fail for division by 0");
+            //if(val2 == 0)
+            //    panic("eval fail for division by 0");
             return val1 / val2;
         case TK_EQ:
             return (uint32_t)(val1 == val2);
