625e5db17d270faece14faccf57572edf2c28985
tracer-ics2020 <tracer@njuics.org>
2024-04-14 17:49:00 +0800

    >  compile 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 8ecd5cc9815c4a15bf0c9046f8364b5f   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  17:49:00 up  5:11,  1 user,  load average: 0.02, 0.06, 0.02

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,10 +1,38 @@
-void init_monitor(int, char *[]);
+#include <debug.h>
+#include <common.h>
+#include "monitor/debug/expr.h"
+
 void engine_start();
 int is_exit_status_bad();
+void init_monitor(int, char *[]);
+
+void test_expr(){
+    FILE* fp = fopen("tools/gen-expr/input", "r");
+    assert(fp!=NULL);
+
+    char  expression[70000];
+    for (int i = 0; i < 10000; i++) {
+        bool flag = true;
+        unsigned ans = 0, eval = 0;
+        int rv = fscanf(fp, "result == %u, %[^\n]", &ans, expression);
+        Assert(rv == 2, "test_expr failed");
+        eval = expr(expression, &flag);
+        Assert(flag == true, "test_expr failed");
+        if (ans != eval){
+            printf("Wrong %d:\n", i);
+            printf("result == %u, eval == %u\n", ans, eval);
+        }
+        else
+            printf("correct %d\n", i);
+    }
+    fclose(fp);
+}
 
 int main(int argc, char* argv[]) {
     /* Initialize the monitor. */
     init_monitor(argc, argv);
+
+    test_expr();
 
     /* Start engine. */
     engine_start();
