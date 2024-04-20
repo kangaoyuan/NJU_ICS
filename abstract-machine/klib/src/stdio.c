ebeb349a772890e82d1832ebb06b2e0e80378176
tracer-ics2020 <tracer@njuics.org>
2024-04-20 22:19:55 +0800

    >  run 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 38e4962691424e88a20e990eb9ff07b1   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  22:19:55 up  9:47,  1 user,  load average: 0.16, 0.04, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,6 +6,39 @@
 #define MAX_BUF 2048
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char digits[] = "0123456789ABCDEF";
+
+static void printint(char* out, int xx, int base, int sgn) {
+    char buf[32];
+    unsigned int x;
+    int  i = 0, neg;
+
+    neg = 0;
+    if (sgn && xx < 0) {
+        neg = 1;
+        x = -xx;
+    } else {
+        x = xx;
+    }
+
+    do {
+        buf[i++] = digits[x % base];
+    } while ((x /= base) != 0);
+    if (neg)
+        buf[i++] = '-';
+
+    while (--i >= 0)
+        *out++ = buf[i];
+}
+
+static void printptr(char* out, uint32_t x) {
+    int i;
+    *out++ = '0';
+    *out++ = 'x';
+    for (i = 0; i < (sizeof(uint32_t) * 2); i++, x <<= 4)
+        *out++ = digits[x >> (sizeof(uint32_t) * 8 - 4)];
+}
+
 int printf(const char *fmt, ...) {
     if(!fmt)
         panic("null fmt in printf");
@@ -19,22 +52,117 @@ int printf(const char *fmt, ...) {
     return rv;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) {
+    if(!fmt)
+        panic("null fmt in printf");
+
+    va_list ap;
+    va_start(ap, fmt);
+    int rv = vsprintf(out, fmt, ap);
+    va_end(ap);
+    return rv;
 }
 
+int vsprintf(char* out, const char* fmt, va_list ap) {
+    char* s;
+    int   c, state = 0, cnt = 0;
 
+    for (int i = 0; fmt[i]; i++) {
+        c = fmt[i] & 0xff;
+        if (state == 0) {
+            if (c == '%') {
+                state = '%';
+            } else {
+                *out++ = c;
+            }
+        } else if (state == '%') {
+            cnt++;
+            if (c == 'd') {
+                printint(out, va_arg(ap, int), 10, 1);
+            } else if (c == 'l') {
+                printint(out, va_arg(ap, unsigned long), 10, 0);
+            } else if (c == 'x') {
+                printint(out, va_arg(ap, int), 16, 0);
+            } else if (c == 'p') {
+                printptr(out, va_arg(ap, unsigned int));
+            } else if (c == 's') {
+                s = va_arg(ap, char*);
+                if (s == 0)
+                    s = "(null)";
+                while (*s != 0) {
+                    *out++ = *s++;
+                }
+            } else if (c == 'c') {
+                *out++ = va_arg(ap, unsigned int);
+            } else if (c == '%') {
+                *out++ = c;
+            } else {
+                // Unknown % sequence.  Print it to draw attention.
+                *out++ = '%';
+                *out++ = c;
+            }
+            state = 0;
+        }
+    }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+    return cnt;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+    if(!fmt)
+        panic("null fmt in printf");
+
+    va_list ap;
+    va_start(ap, fmt);
+    int rv = vsnprintf(out, n, fmt, ap);
+    va_end(ap);
+    return rv;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    // unimplement the n restriction.
+    char* s;
+    int   c, state = 0, cnt = 0;
+
+    for (int i = 0; fmt[i]; i++) {
+        c = fmt[i] & 0xff;
+        if (state == 0) {
+            if (c == '%') {
+                state = '%';
+            } else {
+                *out++ = c;
+            }
+        } else if (state == '%') {
+            cnt++;
+            if (c == 'd') {
+                printint(out, va_arg(ap, int), 10, 1);
+            } else if (c == 'l') {
+                printint(out, va_arg(ap, unsigned long), 10, 0);
+            } else if (c == 'x') {
+                printint(out, va_arg(ap, int), 16, 0);
+            } else if (c == 'p') {
+                printptr(out, va_arg(ap, unsigned int));
+            } else if (c == 's') {
+                s = va_arg(ap, char*);
+                if (s == 0)
+                    s = "(null)";
+                while (*s != 0) {
+                    *out++ = *s++;
+                }
+            } else if (c == 'c') {
+                *out++ = va_arg(ap, unsigned int);
+            } else if (c == '%') {
+                *out++ = c;
+            } else {
+                // Unknown % sequence.  Print it to draw attention.
+                *out++ = '%';
+                *out++ = c;
+            }
+            state = 0;
+        }
+    }
+
+    return cnt;
 }
 
 #endif
