3ea14a1fee1f48f792a3cd886de036566b6f739e
tracer-ics2020 <tracer@njuics.org>
2024-04-22 01:01:14 +0800

    >  compile 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 585bbf1be14f4b35b9264cd11e50da6c   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  01:01:14 up 13:04,  1 user,  load average: 0.10, 0.04, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,24 @@
 #include <am.h>
 #include <nemu.h>
 
+static uint32_t st_time_lo, st_time_hi;
+
 void __am_timer_init() {
+    st_time_lo = inl(RTC_ADDR);
+    st_time_hi = inl(RTC_ADDR+4);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+    uint32_t now_time_lo = inl(RTC_ADDR);
+    uint32_t now_time_hi = inl(RTC_ADDR+4);
+    uptime->us = (now_time_lo - st_time_lo) + (now_time_hi - st_time_hi) * 1000000;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  rtc->second = 0;
-  rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+    rtc->second = 0;
+    rtc->minute = 0;
+    rtc->hour = 0;
+    rtc->day = 0;
+    rtc->month = 0;
+    rtc->year = 1900;
 }
