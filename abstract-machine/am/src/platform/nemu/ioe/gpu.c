755026b9637a71c1bd5928033fc427f0c3e943f6
tracer-ics2020 <tracer@njuics.org>
2024-04-22 15:19:54 +0800

    >  compile 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 7e4573273f3d4b6ebcc85226e54c393a   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  15:19:54 up  3:26,  1 user,  load average: 0.19, 0.06, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,22 +4,43 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-}
-
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
-}
-
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+    int       i;
+    int       w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
+    int       h = inw(VGACTL_ADDR);  // TODO: get the correct height
+    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+    for (i = 0; i < w * h; i++)
+        fb[i] = i;
     outl(SYNC_ADDR, 1);
-  }
+}
+
+void __am_gpu_config(AM_GPU_CONFIG_T* cfg) {
+    *cfg = (AM_GPU_CONFIG_T){.present = true,
+                             .has_accel = false,
+                             .width = inw(VGACTL_ADDR + 2),
+                             .height = inw(VGACTL_ADDR),
+                             .vmemsz = 0};
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T* ctl) {
+    uint32_t* ff;
+    uint32_t width = inw(VGACTL_ADDR+2);
+    uint32_t* pixels = (uint32_t*)ctl->pixels;
+    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+    int       x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+
+
+    for (int j = y; j < y + h; ++j)
+        for (int i = x; i < x + w; ++i) {
+            // printf("%u %x\n",*(ctl->pixels+p),FB_ADDR+i+j*320);
+            ff = fb + (j * width);
+            ff[i] = *pixels++;
+        }
+
+    if (ctl->sync) {
+        outl(SYNC_ADDR, 1);
+    }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
+    status->ready = true;
 }
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -6,12 +6,13 @@
 //#define MODE_800x600
 
 #ifdef MODE_800x600
-# define SCREEN_W 800
-# define SCREEN_H 600
+#   define SCREEN_W 800
+#   define SCREEN_H 600
 #else
-# define SCREEN_W 400
-# define SCREEN_H 300
+#   define SCREEN_W 400
+#   define SCREEN_H 300
 #endif
+
 #define SCREEN_SIZE ((SCREEN_H * SCREEN_W) * sizeof(uint32_t))
 
 #include <device/map.h>
@@ -22,49 +23,58 @@
 #define VGACTL_PORT 0x100 // Note that this is not the standard
 #define VGACTL_MMIO 0xa1000100
 
-static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
+static SDL_Renderer *renderer = NULL;
 
-static uint32_t (*vmem) [SCREEN_W] = NULL;
 static uint32_t *vgactl_port_base = NULL;
+static uint32_t (*vmem) [SCREEN_W] = NULL;
 
+static inline void update_screen() __attribute__((used));
 static inline void update_screen() {
 #ifdef SHOW_SCREEN
-  SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(vmem[0][0]));
-  SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, texture, NULL, NULL);
-  SDL_RenderPresent(renderer);
+    SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(vmem[0][0]));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 #endif
 }
 
 void vga_update_screen() {
-  // TODO: call `update_screen()` when the sync register is non-zero,
-  // then zero out the sync register
+    // TODO: call `update_screen()` when the sync register is non-zero,
+    // then zero out the sync register
+    uint32_t sync = vgactl_port_base[1];
+    if (sync == 1) {
+        update_screen();
+        vgactl_port_base[1] = 0;
+    }
 }
 
 void init_vga() {
 #ifdef SHOW_SCREEN
-  SDL_Window *window = NULL;
-  char title[128];
-  sprintf(title, "%s-NEMU", str(__ISA__));
-  SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = NULL;
+    char        title[128];
+    sprintf(title, "%s-NEMU", str(__ISA__));
+    SDL_Init(SDL_INIT_VIDEO);
 #ifdef MODE_800x600
-  SDL_CreateWindowAndRenderer(SCREEN_W, SCREEN_H, 0, &window, &renderer);
+    SDL_CreateWindowAndRenderer(SCREEN_W, SCREEN_H, 0, &window, &renderer);
 #else
-  SDL_CreateWindowAndRenderer(SCREEN_W * 2, SCREEN_H * 2, 0, &window, &renderer);
+    SDL_CreateWindowAndRenderer(SCREEN_W * 2, SCREEN_H * 2, 0, &window,
+                                &renderer);
 #endif
-  SDL_SetWindowTitle(window, title);
-  texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-      SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
+    SDL_SetWindowTitle(window, title);
+    texture =
+        SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+                          SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
 #endif
 
-  vgactl_port_base = (void *)new_space(8);
-  vgactl_port_base[0] = ((SCREEN_W) << 16) | (SCREEN_H);
-  vgactl_port_base[1] = 1;
-  add_pio_map("screen", VGACTL_PORT, (void *)vgactl_port_base, 8, NULL);
-  add_mmio_map("screen", VGACTL_MMIO, (void *)vgactl_port_base, 8, NULL);
+    vgactl_port_base = (void*)new_space(8);
+    vgactl_port_base[0] = ((SCREEN_W) << 16) | (SCREEN_H);
+    vgactl_port_base[1] = 1;
+    add_pio_map("screen", VGACTL_PORT, (void*)vgactl_port_base, 8, NULL);
+    add_mmio_map("screen", VGACTL_MMIO, (void*)vgactl_port_base, 8, NULL);
 
-  vmem = (void *)new_space(SCREEN_SIZE);
-  add_mmio_map("vmem", VMEM, (void *)vmem, SCREEN_SIZE, NULL);
+    vmem = (void*)new_space(SCREEN_SIZE);
+    add_mmio_map("vmem", VMEM, (void*)vmem, SCREEN_SIZE, NULL);
 }
+
 #endif	/* HAS_IOE */
