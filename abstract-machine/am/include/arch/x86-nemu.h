1aebe4dac01be6a8fba502754382afb2ea6a6946
tracer-ics2020 <tracer@njuics.org>
2024-04-23 15:44:14 +0800

    >  run 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 1481883684da48179ac4b4b019a8a656   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  15:44:14 up  3:20,  1 user,  load average: 0.26, 0.14, 0.04

--- abstract-machine/am/include/arch/x86-nemu.h
+++ abstract-machine/am/include/arch/x86-nemu.h
@@ -4,15 +4,15 @@
 struct Context {
     // TODO: fix the order of these members to match trap.S
     void*     cr3;
-    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-    uint32_t       irq;
-    uint32_t eip, cs, eflags;
+    uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uintptr_t irq;
+    uintptr_t eip, cs, eflags;
 };
 
 #define GPR1 eax
-#define GPR2 eip
-#define GPR3 eip
-#define GPR4 eip
-#define GPRx eip
+#define GPR2 ebx
+#define GPR3 ecx
+#define GPR4 edx
+#define GPRx eax
 
 #endif
