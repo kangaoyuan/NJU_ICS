021536d33c9403475c14d6804c0142b5efe1e52f
tracer-ics2020 <tracer@njuics.org>
2024-04-23 21:59:36 +0800

    >  run 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 1481883684da48179ac4b4b019a8a656   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  21:59:36 up  9:35,  1 user,  load average: 0.02, 0.02, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,14 +9,39 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void* buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+    //TODO();
+
+    // Get Ehdr.
+    Elf_Ehdr elf_header;
+    int rv = ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+    assert(rv == sizeof(Elf_Ehdr));
+
+    // Get Phdr.
+    Elf_Phdr pro_header[elf_header.e_phnum];
+    rv = ramdisk_read(pro_header, elf_header.e_phoff, sizeof(Elf_Phdr)*elf_header.e_phnum);
+    assert(rv == sizeof(Elf_Phdr)*elf_header.e_phnum);
+
+    for (int i = 0; i < elf_header.e_phnum; i++) {
+        Elf_Phdr p = pro_header[i];
+        if (p.p_type == PT_LOAD) {
+            // for Mem, from p_vaddr to p_vaddr + p_memsz.
+            rv = ramdisk_read((void*)p.p_vaddr, p.p_offset, p.p_memsz);
+            assert(rv == p.p_memsz);
+            // for ELF file, from p_offset to p_offset + p_filesz.
+            memset((void*)(p.p_vaddr + p.p_filesz), 0,
+                   p.p_memsz - p.p_filesz);
+        }
+    }
+
+    return elf_header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+    uintptr_t entry = loader(pcb, filename);
+    Log("Jump to entry = %p", entry);
+    ((void (*)())entry)();
 }
 
