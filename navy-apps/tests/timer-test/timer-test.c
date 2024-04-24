c51277e64b50dc7b214128f50e07c066966d86dd
tracer-ics2020 <tracer@njuics.org>
2024-04-24 15:16:05 +0800

    >  run 201402010101  Static hostname: gaoyuankan        Icon name: computer-container          Chassis: container       Machine ID: 4533e83c663e419db1afc8fd49999716          Boot ID: 60fdc1ce4606423ca04b65984730d35d   Virtualization: wsl Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 5.15.146.1-microsoft-standard-WSL2     Architecture: x86-64  15:16:05 up  3:24,  1 user,  load average: 0.23, 0.09, 0.06

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -3,24 +3,58 @@
 #include <assert.h>
 #include <sys/time.h>
 
-int main() {
-    struct timeval init, now;
+#ifndef HAS_NDL
+#include <assert.h>
+#include <sys/time.h>
+
+void gettimeofday_test() {
+    printf("gettimeofday test start...\n");
+
+    struct timeval init;
+    struct timeval now;
 
     assert(gettimeofday(&init, NULL) == 0);
     time_t init_sec = init.tv_sec;
     suseconds_t init_usec = init.tv_usec;
 
-    int times = 1;
+    unsigned int times = 1;
 
     while (1) {
         assert(gettimeofday(&now, NULL) == 0);
         time_t now_sec = now.tv_sec;
         suseconds_t now_usec = now.tv_usec;
-
         uint64_t time_gap = (now_sec - init_sec) * 1000000 + (now_usec - init_usec); // unit: us
         if (time_gap > 500000 * times) {
+            printf("Half a second passed, %u time(s)\n", times);
+            times++;
+        }
+    }
+}
+#else
+#include <NDL.h>
+
+void NDL_GetTicks_test() {
+  NDL_Init(0);
+    printf("NDL_GetTicks test start...\n");
+
+    uint32_t init = NDL_GetTicks();
+    size_t times = 1;
+
+    while (1) {
+        uint32_t now = NDL_GetTicks();
+        uint32_t time_gap = now - init;
+        if (time_gap > 500 * times) {
             printf("Half a second passed, %d time(s)\n", times);
             times++;
         }
     }
+}
+#endif
+
+int main() {
+#ifndef HAS_NDL
+    gettimeofday_test();
+#else
+    NDL_GetTicks_test();
+#endif
 }
