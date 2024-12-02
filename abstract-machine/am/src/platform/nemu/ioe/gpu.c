7044b584151fa349d2062b144a71e7499e96b153
tracer-ics2020 <tracer@njuics.org>
2024-12-02 16:31:52 +0800

    >  run 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: 94d2188da7b74661a0b53bb7408b1e07 Operating System: Arch Linux           Kernel: Linux 6.12.1-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 5month 2w 2d  16:31:52 up 2 days, 14:31,  2 users,  load average: 1.35, 1.19, 0.97

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,6 +4,7 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+    // Below code is for testing.
     int       w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
     int       h = inw(VGACTL_ADDR);  // TODO: get the correct height
     uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
@@ -17,7 +18,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T* cfg) {
                              .has_accel = false,
                              .width = inw(VGACTL_ADDR + 2),
                              .height = inw(VGACTL_ADDR),
-                             .vmemsz = 0};
+                             .vmemsz = cfg->width * cfg->height * 4};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T* ctl) {
@@ -28,6 +29,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T* ctl) {
     int       x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
 
 
+    // Here I'm not accounting for the edge problem.
     for (int j = y; j < y + h; ++j)
         for (int i = x; i < x + w; ++i) {
             // printf("%u %x\n",*(ctl->pixels+p),FB_ADDR+i+j*320);
@@ -42,4 +44,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T* ctl) {
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
     status->ready = true;
+}
+
+// It's only for learning the workflow
+void __am_gpu_memcpy(AM_GPU_MEMCPY_T *cpy){
+    
 }
