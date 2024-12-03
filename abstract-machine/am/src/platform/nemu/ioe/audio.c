5c3e08608ef1e2682b6566a6eb61e48bfb8d1e00
tracer-ics2020 <tracer@njuics.org>
2024-12-03 15:57:20 +0800

    >  compile 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: 86d215bfac694ab4a980fc363a43c684 Operating System: Arch Linux           Kernel: Linux 6.12.1-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 5month 2w 3d  15:57:20 up 36 min,  2 users,  load average: 1.33, 2.05, 1.12

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,19 +8,52 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+/*
+ * AM_AUDIO_CONFIG_T { bool present; int bufsize }
+ * AM_AUDIO_CTRL_T   { int freq, channels, samples }
+ * AM_AUDIO_STATUS_T { int count }
+ * AM_AUDIO_PLAY_T   { Area buf }
+ */
+
 void __am_audio_init() {
+    /* It be done inside the nemu to start up */
+    //outl(AUDIO_SBUF_SIZE_ADDR, 1<<16);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+
+  outl(AUDIO_COUNT_ADDR, 0);
+  outl(AUDIO_INIT_ADDR, true);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  Area buf = ctl->buf;
+  int len = buf.end - buf.start;
+  uint8_t* audio_data = buf.start;
+  int cnt = inl(AUDIO_COUNT_ADDR);
+  int sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+
+  while(len + cnt > sbuf_size){
+    ;
+    cnt = inl(AUDIO_COUNT_ADDR); 
+  }
+
+  uint8_t* audio_base = (uint8_t*)(uintptr_t)(AUDIO_SBUF_ADDR);
+  for(int i = 0; i < len; ++i){
+    audio_base[cnt+i] = audio_data[i]; 
+  }
+
+  outl(AUDIO_COUNT_ADDR, cnt);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -23,19 +23,67 @@ enum {
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
+// SDL callback function to fill audio data from MMIO STREAM_BUF to SDL.
 static inline void audio_play(void *userdata, uint8_t *stream, int len) {
+    SDL_memset(stream, 0, len);
+    uint32_t cnt = audio_base[reg_count];
+    uint32_t size = audio_base[reg_sbuf_size];
+
+    int nread = len > cnt ? cnt : len;
+    for(int index = 0; index < nread; index++){
+        if(audio_base[reg_count]){
+            *stream++ = sbuf[index % size]; 
+            audio_base[reg_count]--;
+        }
+    }
+    for(int i = 0; i < cnt - nread; i++){
+        sbuf[i] = sbuf[i + nread]; 
+    }
+
+    // It be done from the first statement.
+    /*
+     *if(len > cnt){
+     *    memset(stream, 0, len - cnt); 
+     *}
+     */
+}
+
+static void init_audio_sdl(){
+    SDL_AudioSpec s = {};
+    s.format = AUDIO_S16SYS;
+    s.userdata = NULL;
+
+    s.freq = audio_base[reg_freq];
+    s.channels = audio_base[reg_channels]; 
+    s.samples = audio_base[reg_samples];
+    s.callback = audio_play;
+
+    int rc = SDL_InitSubSystem(SDL_INIT_AUDIO);
+    if(rc == 0) {
+        SDL_OpenAudio(&s, NULL);
+        SDL_PauseAudio(0);
+    }
 }
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+    if(audio_base[reg_init] == false){
+        init_audio_sdl(); 
+        audio_base[reg_init] = true;
+    }
 }
 
+
 void init_audio() {
-  uint32_t space_size = sizeof(uint32_t) * nr_reg;
+  uint32_t space_size = nr_reg * sizeof(uint32_t);
   audio_base = (void *)new_space(space_size);
   add_pio_map("audio", AUDIO_PORT, (void *)audio_base, space_size, audio_io_handler);
   add_mmio_map("audio", AUDIO_MMIO, (void *)audio_base, space_size, audio_io_handler);
+  audio_base[reg_count] = 0;
+  audio_base[reg_init] = false;
+  audio_base[reg_sbuf_size] = STREAM_BUF_MAX_SIZE;
 
   sbuf = (void *)new_space(STREAM_BUF_MAX_SIZE);
   add_mmio_map("audio-sbuf", STREAM_BUF, (void *)sbuf, STREAM_BUF_MAX_SIZE, NULL);
 }
+
 #endif	/* HAS_IOE */
