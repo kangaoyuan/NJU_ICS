8835f2a1453feb57f0c7530a3a1265b5bce4a1ee
tracer-ics2020 <tracer@njuics.org>
2024-12-12 14:33:17 +0800

    >  run 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: f043d82a01f6484e8dcbb177561de44e Operating System: Arch Linux           Kernel: Linux 6.12.1-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 5month 3w 5d  14:33:17 up  1:49,  2 users,  load average: 0.62, 0.26, 0.19

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -13,13 +13,14 @@
  *} SDL_Rect;
  */
 
-typedef struct SDL_Color{
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-    uint8_t unused;
-} SDL_Color;
-
+/*
+ *typedef struct SDL_Color{
+ *    uint8_t r;
+ *    uint8_t g;
+ *    uint8_t b;
+ *    uint8_t unused;
+ *} SDL_Color;
+ */
 
 /*
  *typedef struct {
