8c075ff2dc6a99c1a13530585f18600fd847c0b7
tracer-ics2020 <tracer@njuics.org>
2024-12-12 16:05:11 +0800

    >  run 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: f043d82a01f6484e8dcbb177561de44e Operating System: Arch Linux           Kernel: Linux 6.12.1-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 5month 3w 5d  16:05:11 up  3:20,  2 users,  load average: 0.18, 0.27, 0.34

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,12 +5,14 @@
 
 void __am_gpu_init() {
     // Below code is for testing.
-    int       w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
-    int       h = inw(VGACTL_ADDR);  // TODO: get the correct height
-    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-    for (int i = 0; i < w * h; i++)
-        fb[i] = i;
-    outl(SYNC_ADDR, 1);
+    /*
+     *int       w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
+     *int       h = inw(VGACTL_ADDR);  // TODO: get the correct height
+     *uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+     *for (int i = 0; i < w * h; i++)
+     *    fb[i] = i;
+     *outl(SYNC_ADDR, 1);
+     */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T* cfg) {
