8a5270049ec690c0ef539f7e2143c13e0a2df3cc
tracer-ics2020 <tracer@njuics.org>
2024-12-12 16:26:47 +0800

    >  run 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: f043d82a01f6484e8dcbb177561de44e Operating System: Arch Linux           Kernel: Linux 6.12.1-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 5month 3w 5d  16:26:47 up  3:42,  2 users,  load average: 0.34, 0.31, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -20,27 +20,43 @@ void __am_gpu_config(AM_GPU_CONFIG_T* cfg) {
                              .has_accel = false,
                              .width = inw(VGACTL_ADDR + 2),
                              .height = inw(VGACTL_ADDR),
-                             .vmemsz = inw(VGACTL_ADDR + 2) * inw(VGACTL_ADDR) * 4};
+                             .vmemsz = cfg->width * cfg->height * 4};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T* ctl) {
-    uint32_t* ff;
-    uint32_t width = inw(VGACTL_ADDR+2);
-    uint32_t* pixels = (uint32_t*)ctl->pixels;
-    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-    int       x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-
-
-    for (int j = y; j < y + h; ++j)
-        for (int i = x; i < x + w; ++i) {
-            // printf("%u %x\n",*(ctl->pixels+p),FB_ADDR+i+j*320);
-            ff = fb + (j * width);
-            ff[i] = *pixels++;
-        }
-
-    if (ctl->sync) {
-        outl(SYNC_ADDR, 1);
+/*
+ *    uint32_t* ff;
+ *    uint32_t width = inw(VGACTL_ADDR+2);
+ *    uint32_t* pixels = (uint32_t*)ctl->pixels;
+ *    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+ *    int       x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+ *
+ *
+ *    for (int j = y; j < y + h; ++j)
+ *        for (int i = x; i < x + w; ++i) {
+ *            // printf("%u %x\n",*(ctl->pixels+p),FB_ADDR+i+j*320);
+ *            ff = fb + (j * width);
+ *            ff[i] = *pixels++;
+ *        }
+ *
+ *    if (ctl->sync) {
+ *        outl(SYNC_ADDR, 1);
+ *    }
+ */
+      int h = inl(VGACTL_ADDR)&0xffff;
+  int w = inl(VGACTL_ADDR)>>16;
+  int x = ctl->x, y = ctl->y;
+  //printf("w is %d\n",h);
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb  = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (int j = y; j<h && j<(y+ctl->h);j++){
+    for (int i = x;i<w && i<(x + ctl->w);i++){
+      *(fb+j*w+i) = *pixels++;
     }
+  }
+  if(ctl->sync){
+    outl(SYNC_ADDR,1);
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
