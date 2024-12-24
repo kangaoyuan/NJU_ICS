30528273234b5e002d0c83bf3ca8b42e2955f051
tracer-ics2020 <tracer@njuics.org>
2024-12-25 00:35:54 +0800

    >  compile 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: f564c469d5ba4898b82fc97af083f4bf Operating System: Arch Linux           Kernel: Linux 6.12.1-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 6month 1w 1d  00:35:54 up 1 day, 13:01,  2 users,  load average: 1.20, 1.17, 1.18

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -4,7 +4,7 @@
 
 #define PT_P 0x1
 
-paddr_t page_table_walk(vaddr_t vaddr){
+paddr_t page_table_walk(vaddr_t vaddr) {
     uint32_t va_pd = (vaddr >> 22);
     uint32_t va_pg = (vaddr >> 12) & ((1 << 10) - 1);
 
@@ -43,27 +43,38 @@ paddr_t page_table_walk(vaddr_t vaddr){
  *}
  */
 
-paddr_t vaddr_read_cross_page(vaddr_t vaddr ,int type,int len){
-  paddr_t paddr = page_table_walk(vaddr);
-  uint32_t offset = vaddr&0xfff;
-  uint32_t partial = offset + len - PAGE_SIZE;
-  uint32_t low=0,high =0;
-  if(len - partial == 3)
-  {
-    low = paddr_read(paddr,4)&0xffffff;
-  }
-  else low = paddr_read(paddr,len - partial);
-  if(partial == 3)
-  {
-    high = paddr_read(page_table_walk((vaddr&(~0xfff)) + PAGE_SIZE),4)&0xffffff;
-  }
-  else high = paddr_read(page_table_walk((vaddr&(~0xfff)) + PAGE_SIZE),partial);
-  //printf("pc = %x:offset = %d base = %x :cross read = %x partial = %d, high = %x, low = %x\n",cpu.pc,offset,cpu.CR3,((high << 8*(len-partial))|low),partial,high,low);
-  /* assert(len - partial != 3&&partial != 3);
-  low = paddr_read(paddr,len - partial);
-  high = paddr_read(page_table_walk((vaddr&0xfff) + PAGE_SIZE),partial); */
-  //printf("cross read %x\n",((high << 8*(len-partial))|low));
-  return ((high << 8*(len-partial))|low);
+paddr_t vaddr_read_cross_page(vaddr_t vaddr, int type __attribute__((unused)), int len) {
+    paddr_t  paddr = page_table_walk(vaddr);
+    uint32_t offset = vaddr & 0xfff;
+    uint32_t partial = offset + len - PAGE_SIZE;
+    assert(partial > 0);
+    uint32_t low = 0, high = 0;
+    if (len - partial == 3) {
+        low = paddr_read(paddr, 4) & 0xffffff;
+    } else {
+        low = paddr_read(paddr, len - partial);
+    }
+
+    /*
+     *for(uint32_t i = 0; i < len - partial; ++i){
+     *    low = (low << 8) + paddr_read(paddr+i, 1); 
+     *}
+     */
+
+    if (partial == 3) {
+        high = paddr_read(page_table_walk((vaddr & (~0xfff)) + PAGE_SIZE), 4) & 0xffffff;
+    } else {
+        high = paddr_read(page_table_walk((vaddr & (~0xfff)) + PAGE_SIZE), partial);
+    }
+    // printf("pc = %x:offset = %d base = %x :cross read = %x partial = %d,
+    // high = %x, low = %x\n",cpu.pc,offset,cpu.CR3,((high <<
+    // 8*(len-partial))|low),partial,high,low);
+    /* assert(len - partial != 3&&partial != 3);
+    low = paddr_read(paddr,len - partial);
+    high = paddr_read(page_table_walk((vaddr&0xfff) + PAGE_SIZE),partial);
+  */
+    // printf("cross read %x\n",((high << 8*(len-partial))|low));
+    return ((high << 8 * (len - partial)) | low);
 }
 
 word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
@@ -100,16 +111,18 @@ word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
      */
 }
 
-void vaddr_write_cross_page(vaddr_t vaddr ,word_t data,int len)
-{
-  uint8_t a[4] = {data&0xff,(data>>8)&0xff,(data>>16)&0xff,(data>>24)&0xff};
-  for(int i = 0;i < len;i ++)
-  {
-    vaddr_write(page_table_walk(vaddr+i),a[i],1);
-    printf("%x",a[i]);
-  }
-  printf("\n");
-}
+/*
+ *void vaddr_write_cross_page(vaddr_t vaddr ,word_t data,int len)
+ *{
+ *  uint8_t a[4] = {data&0xff,(data>>8)&0xff,(data>>16)&0xff,(data>>24)&0xff};
+ *  for(int i = 0;i < len;i ++)
+ *  {
+ *    vaddr_write(page_table_walk(vaddr+i),a[i],1);
+ *    printf("%x",a[i]);
+ *  }
+ *  printf("\n");
+ *}
+ */
 
 void vaddr_mmu_write(vaddr_t addr, word_t data, int len){
 /*
@@ -143,6 +156,8 @@ void vaddr_mmu_write(vaddr_t addr, word_t data, int len){
      *}
      */
 }
+
+
 
 /*
  *paddr_t isa_mmu_translate(vaddr_t vaddr, int type [[maybe_unused]],
