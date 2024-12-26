03a80fb61fe95118d123dc231a600f16457257a5
tracer-ics2020 <tracer@njuics.org>
2024-12-26 22:05:57 +0800

    >  run 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: 44dc4418c8bf41b393099b7c503adf8b Operating System: Arch Linux           Kernel: Linux 6.12.1-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 6month 1w 2d  22:05:57 up  9:28,  2 users,  load average: 0.68, 0.48, 0.31

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -63,40 +63,42 @@ void map(AddrSpace* as, void* va, void* pa, int prot);
  *}
  */
 
-#define OFFSET 0xfff
-#define PGSIZE 4096
-#define page_begin_vaddr(A) ((void*)((uintptr_t)(A)&(~OFFSET)))
-#define page_end_vaddr(A) ((void*)(((uintptr_t)(A)&(~OFFSET))+PGSIZE))
-#define min(A,B) ((size_t)(A) < (size_t)(B)? (A):(B))
-#define vaddr_offset(A) ((uintptr_t)(A)&OFFSET)
-int mm_brk(uintptr_t brk)
-{
-  if (current->max_brk >= brk)
-  {
-    return 0;
-  }
-  else
-  {
-    void *vaddr = (void *)current->max_brk;
-    void *end = (void *)brk;
-    void *paddr = NULL;
-    uintptr_t len = (uintptr_t)min(page_end_vaddr(vaddr) - vaddr, end - vaddr);
-    if(len < PGSIZE) {
-      vaddr += len;
-    } else if(len == PGSIZE) {
-      assert(vaddr_offset(vaddr) == 0);
-    } else assert(0);
-    while((uintptr_t)vaddr < (uintptr_t)end)
-    {
-      len = (uintptr_t)min(page_end_vaddr(vaddr) - vaddr, end - vaddr);
-      paddr = new_page(1);
-      map(&current->as,page_begin_vaddr(vaddr),paddr,0);
-      vaddr += len;
-    }
-    current->max_brk = brk;
-  }
-  return 0;
-}
+/*
+ *#define OFFSET 0xfff
+ *#define PGSIZE 4096
+ *#define page_begin_vaddr(A) ((void*)((uintptr_t)(A)&(~OFFSET)))
+ *#define page_end_vaddr(A) ((void*)(((uintptr_t)(A)&(~OFFSET))+PGSIZE))
+ *#define min(A,B) ((size_t)(A) < (size_t)(B)? (A):(B))
+ *#define vaddr_offset(A) ((uintptr_t)(A)&OFFSET)
+ *int mm_brk(uintptr_t brk)
+ *{
+ *  if (current->max_brk >= brk)
+ *  {
+ *    return 0;
+ *  }
+ *  else
+ *  {
+ *    void *vaddr = (void *)current->max_brk;
+ *    void *end = (void *)brk;
+ *    void *paddr = NULL;
+ *    uintptr_t len = (uintptr_t)min(page_end_vaddr(vaddr) - vaddr, end - vaddr);
+ *    if(len < PGSIZE) {
+ *      vaddr += len;
+ *    } else if(len == PGSIZE) {
+ *      assert(vaddr_offset(vaddr) == 0);
+ *    } else assert(0);
+ *    while((uintptr_t)vaddr < (uintptr_t)end)
+ *    {
+ *      len = (uintptr_t)min(page_end_vaddr(vaddr) - vaddr, end - vaddr);
+ *      paddr = new_page(1);
+ *      map(&current->as,page_begin_vaddr(vaddr),paddr,0);
+ *      vaddr += len;
+ *    }
+ *    current->max_brk = brk;
+ *  }
+ *  return 0;
+ *}
+ */
 
 void init_mm() {
     pf = (void*)ROUNDUP(heap.start, PGSIZE);
